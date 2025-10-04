29654f41650d9dbbc09981c410e68930ac2a8a9f
sivaraj c <132218792+Sivarajc2005@users.noreply.github.com>
2025-10-04 10:55:34 +0530

    Time: 4 ms (91.83%), Space: 57.8 MB (66.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,13 +3,9 @@ class Solution {
         int n = height.length;
         int st = 0;
         int en = n -1;
-        int left = height[st];
-        int right = height[en];
         int ans = Integer.MIN_VALUE;
         while(st <= en){
             if(Math.min(height[st] , height[en])*(en - st) > ans){
-                // System.out.println("en : "+en+" st : "+st+" = "+(en - st));
-                // System.out.println(Math.min(height[st] , height[en]));
                 ans = Math.min(height[st] , height[en])*(en - st);
             }
             if(height[st] <= height[en]){
