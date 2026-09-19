a6af08902d2022efb43a0b21f76d37355852c1ec
sivaraj c <132218792+Sivarajc2005@users.noreply.github.com>
2026-09-19 19:46:04 +0530

    Time: 1 ms (71.72%), Space: 43.9 MB (15.06%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,25 +13,88 @@
  *     }
  * }
  */
-
- // it's BFS 
-public class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        rightView(root, result, 0);
-        return result;
-    }
+class Solution {
     
-    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
-        if(curr == null){
-            return;
+    class Sol {
+        int place;
+        TreeNode node;
+
+        public Sol(int place, TreeNode node) {
+            this.place = place;
+            this.node = node;
         }
-        if(currDepth == result.size()){
-            result.add(curr.val);
-        }
-        
-        rightView(curr.right, result, currDepth + 1);
-        rightView(curr.left, result, currDepth + 1);
-        
     }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        List<Sol> sol = new ArrayList<>();
+        sol.add(new Sol(0, root));
+        sol = rec(root, sol, 0);
+
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) {
+            return ans;
+        }
+        // ans.add(sol.get(0).node.val);
+
+        // for(int i = 0; i < sol.size(); i++) {
+        //     Sol curr = sol.get(i);
+        //     System.out.println("curr val: { place: " + curr.place + " node: "+ curr.node.val + " }");
+        // }
+        if(sol.size() == 1) {
+            ans.add(root.val);
+            return ans;
+        }
+        for(int i = 1; i < sol.size(); i++) {
+            Sol curr = sol.get(i);
+            Sol pre = sol.get(i-1);
+
+            // System.out.println("pre val: { place: " + pre.place + " node: "+ pre.node.val + " }");
+            // System.out.println("curr val: { place: " + curr.place + " node: "+ curr.node.val + " }");
+
+            if(pre.place != curr.place) {
+                ans.add(pre.node.val);
+
+                // System.out.println("Added pre: "+ pre.node.val);
+
+                // if(i == sol.size() - 1 && sol.size() != 1) {
+                //     ans.add(curr.node.val);
+                //     // System.out.println("Added curr: "+ curr.node.val);
+                
+                // }
+            }
+
+            if(i == sol.size() - 1 && sol.size() != 1) {
+                ans.add(curr.node.val);
+                // System.out.println("Added curr: "+ curr.node.val);
+            
+            }
+        }
+
+        return ans;
+    }
+
+    public List<Sol> rec(TreeNode node, List<Sol> sol, int currInd) {
+        if(node == null) {
+            return sol;
+        }
+
+        Sol curr = sol.get(currInd);
+
+        if(node.left != null) {
+            sol.add(new Sol(curr.place + 1, node.left));
+        }
+        if(node.right != null) {
+            sol.add(new Sol(curr.place + 1, node.right));
+        }
+        
+
+        if(currInd != sol.size() - 1) {
+           sol = rec(sol.get(currInd + 1).node, sol, currInd + 1);
+        }
+
+        return sol;
+    }
+
+    
 }
