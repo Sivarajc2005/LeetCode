90d2e4eaaa1a3929bf057d85cd797765bd5123d6
sivaraj c <132218792+Sivarajc2005@users.noreply.github.com>
2026-09-23 20:03:16 +0530

    Time: 155 ms (4.57%), Space: 46.9 MB (6.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        } else if( x < 10) {
+            return true;
+        }
+
+        int bef = x;
+        int aft = 0;
+
+        boolean isFirst = true;
+        boolean isZeroAval = false;
+
+        while(x != 0) {
+            // extract last ele
+            int temp = x % 10;
+
+            if(isFirst && temp == 0) {
+                isZeroAval = true;
+            }
+
+            // without last ele
+            bef = x / 10;
+
+            if(bef == 0) {
+                return false;
+            }
+
+            // used for odd one we leav the mid one and check
+            print("bef: "+ bef + " aft: "+ aft+ " temp: "+ temp);
+            if(bef == aft) {
+                if(isZeroAval) {
+                    return false;
+                }
+                print("sol on odd");
+                return true;
+            }
+
+            aft = (aft * 10 ) + temp;
+
+            
+            if(bef == aft) {
+                if(isZeroAval) {
+                    return false;
+                }
+                print("bef: "+ bef + " aft: "+ aft+ " temp: "+ temp);
+                print("sol on odd");
+                return true;
+            }
+
+            // red x
+            x /= 10;
+
+            isFirst = false;
+        }
+
+        return false;
+    }
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+}
